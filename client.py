b5c6d138d69209a5df2a63f95e2f9b461e9cf96c
Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2023-05-07 12:35:49 +0700

    yes update final

--- client.py
+++ client.py
@@ -1,19 +1,44 @@
 from socket import *
 import sys
 
+# Mengecek jumlah argumen yang diberikan
 if len(sys.argv) < 4:
-    print('client.py server_host server_port filename')
+    print("Usage: python client.py server_host server_port filename")
     sys.exit()
 
-serverName = sys.argv[1]
+# Membaca argumen dari baris perintah
+serverHost = sys.argv[1]
 serverPort = int(sys.argv[2])
 filename = sys.argv[3]
+
+# Membuat socket client
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-clientSocket.send(f'GET /{filename} HTTP/1.1'.encode())
-response = clientSocket.recv(1024)
-while (response):
-    print(response.decode())
-    response = clientSocket.recv(1024)
-clientSocket.close()
-sys.exit()
+
+try:
+    # Melakukan koneksi ke server
+    clientSocket.connect((serverHost, serverPort))
+
+    # Membuat HTTP request
+    request = f"GET /{filename} HTTP/1.1\r\n"
+    request += f"Host: {serverHost}\r\n\r\n"
+
+    # Mengirimkan HTTP request ke server
+    clientSocket.send(request.encode())
+
+    # Menerima respons dari server
+    response = ""
+    while True:
+        data = clientSocket.recv(1024)
+        if not data:
+            break
+        response += data.decode()
+
+    # Mencetak HTTP response
+    print(response)
+
+except ConnectionRefusedError:
+    print("Error: Connection Refused.")
+
+finally:
+    # Menutup socket client
+    clientSocket.close()
--- server.py
+++ server.py
@@ -3,52 +3,52 @@
 from socket import *
 import sys
 
-# membuat socket server
+# Membuat socket server
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverPort = 6789
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 
 while True:
+    # Menunggu koneksi dari client
     print('Ready to serve...')
-    # membuat koneksi baru dari client
+    # Membuat socket baru untuk koneksi dengan client
     connectionSocket, addr = serverSocket.accept()
     try:
+        # Menerima dan memparsing HTTP request dari client
         message = connectionSocket.recv(1024).decode()
         request = message.split()
 
-        # mengecek apakah request method GET
+        # Mengecek apakah request method adalah GET
         if len(request) < 2 or request[0] != "GET":
             raise IOError
 
-        # mengambil nama file dan mencoba membuka file
+        # Mengambil nama file yang diminta oleh client
         filename = request[1][1:]
-        with open(filename) as f:
+
+        # Membuka file yang diminta oleh client
+        with open(filename, 'rb') as f:
             outputdata = f.read()
 
         # HTTP response header jika file ditemukan
-        connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
-        connectionSocket.send("Content-Type: text/html\r\n".encode())
-        connectionSocket.send(
-            "Content-Length: {}\r\n".format(len(outputdata)).encode())
-        connectionSocket.send("\r\n".encode())
+        response_header = "HTTP/1.1 200 OK\r\n"
+        response_header += "Content-Type: text/html\r\n"
+        response_header += f"Content-Length: {len(outputdata)}\r\n\r\n"
 
-        # mengirim konten file ke client
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode())
+        # Mengirim HTTP response message ke client
+        connectionSocket.send(response_header.encode())
+        connectionSocket.sendall(outputdata)
 
-        # menutup socket client
-        connectionSocket.close()
     except IOError:
-        # HTTP header untuk file not found atau request method bukan GET
-        connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())
-        connectionSocket.send("Content-Type: text/html\r\n".encode())
-        connectionSocket.send("\r\n".encode())
-        connectionSocket.send(
-            "<html><head></head><body><h1>404 Not Found</h1></body></html>".encode())
+        # Mengirim response "404 Not Found" jika file tidak ditemukan
+        response_header = "HTTP/1.1 404 Not Found\r\n"
+        response_header += "Content-Type: text/html\r\n\r\n"
+        response_message = "<html><head></head><body><h1>404 Not Found</h1></body></html>"
+        connectionSocket.send((response_header + response_message).encode())
 
-        # menutup socket client
-        connectionSocket.close()
+    # Menutup koneksi dengan client
+    connectionSocket.close()
 
+# Menutup socket server dan keluar dari program
 serverSocket.close()
 sys.exit()
