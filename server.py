316077699a2753c05c87b1f15123b26f2aff3688
Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2023-05-19 22:01:41 +0700

    upload file done

--- server.py
+++ server.py
@@ -2,6 +2,9 @@
 
 from socket import *
 import sys
+from streaming_form_data import StreamingFormDataParser
+from streaming_form_data.targets import FileTarget
+import os
 
 # Membuat socket server
 serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -15,7 +18,7 @@ while True:
     # Membuat socket baru untuk koneksi dengan client
     connectionSocket, addr = serverSocket.accept()
     try:
-        # Menerima dan memparsing HTTP request dari client
+        # Menerima HTTP request dari client
         chunk = []
         while True:
             data = connectionSocket.recv(1024)
@@ -23,17 +26,24 @@ while True:
             if not data or len(data) < 1024:
                 break
         message = (b''.join(chunk)).decode()
+
+        # Memparsing HTTP request
         request_header, request_body = message.split('\r\n\r\n', 1)
-        request_header = request_header.split()
+        request_header = request_header.split('\r\n')
+        request_method, request_header = request_header[0].split(), request_header[1:]
+        headers = {}
+        for header in request_header:
+            key, value = header.split(':', 1)
+            headers[key] = value.strip()
 
         # Mengecek apakah request method
-        if len(request_header) < 2:
+        if len(request_method) < 2:
             raise IOError
 
-        if request_header[0] == "GET":
+        if request_method[0] == "GET":
             # Jika request method GET
             # Mengambil nama file yang diminta oleh client
-            filename = request_header[1][1:]
+            filename = request_method[1][1:]
 
             # Membuka file yang diminta oleh client
             with open(filename, "rb") as f:
@@ -47,14 +57,24 @@ while True:
             # Mengirim HTTP response message ke client
             connectionSocket.send(response_header.encode())
             connectionSocket.sendall(outputdata)
-        elif request_header[0] == "POST":
-            # # Jika request method POST
-            # # HTTP response header
-            # response_header = "HTTP/1.1 200 OK\r\n"
-            # response_header = f"Location: /{filename}\r\n\r\n"
+        elif request_method[0] == "POST":
+            # Jika request method POST
+            # Parsing form data
+            parser = StreamingFormDataParser(headers=headers)
+            target = FileTarget('tmpfile')
+            parser.register('file', target)
+            parser.data_received(request_body.encode())
+            try:
+                os.rename(target.filename, target.multipart_filename)
+            except:
+                os.remove(target.filename)
 
-            # # Mengirim HTTP response message ke client
-            # connectionSocket.send(response_header.encode())
+            # HTTP response header
+            response_header = "HTTP/1.1 303 See Other\r\n"
+            response_header += f"Location: {target.multipart_filename}\r\n\r\n"
+
+            # Mengirim HTTP response message ke client
+            connectionSocket.send(response_header.encode())
             pass
 
 
